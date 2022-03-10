e876a3685448642965a348dbed13b7f2041c1715
Sanjeev Dasgupta <sanjeev_dasgupta@hotmail.com>
2022-03-10 21:39:08 +0530

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -1,4 +1,4 @@
-const Sequelize = require('sequelize');
+const {Sequelize} = require('sequelize');
 
 const sequelize = new Sequelize('sql6478058', 'sql6478058', '3VIQYqW9Yc', {
     dialect: 'mysql',
