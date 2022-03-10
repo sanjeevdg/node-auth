ef778924a53166bf29af6d04414bb3f189dc4f51
Sanjeev Dasgupta <sanjeev_dasgupta@hotmail.com>
2022-03-10 20:41:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require("express");
-
+const http = require("http");
 const sequelize = require('./utils/database.js');
 
 const router = require('./routes/routes.js');
 
 const app = express();
+
 app.set('port', (process.env.PORT || 5000));
+
+
+
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
+
+
 
 app.use((_, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -19,8 +25,10 @@ app.use((_, res, next) => {
 
 app.use(router);
 
+const server = http.createServer(app);
+
 sequelize.sync(); 
 
-app.listen(app.get('port'), function() {
+server.listen(app.get('port'), function() {
     console.log('App is running, server is listening on port ', app.get('port'));
 });
