a5a7a6d4d81004fd3dfd12f06c538aea13838c6c
Sanjeev Dasgupta <sanjeev_dasgupta@hotmail.com>
2022-03-11 09:33:25 +0530

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -9,7 +9,7 @@ const router = express.Router();
 //function(req, res){
 router.post('/login',function(req, res){ login;});
 
-router.post('/signup',function(req, res,err){ signup;console.log('myerr:::'+err);});
+router.post('/signup',function(req, res,err){ signup;console.log('myerr:::'+err.message);});
 
 //router.get('/signup',function(req, res){
 // signup
