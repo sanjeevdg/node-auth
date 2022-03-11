cf70901bfcb59b0ebf28201531730766b86234da
Sanjeev Dasgupta <sanjeev_dasgupta@hotmail.com>
2022-03-11 12:03:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('port', (process.env.PORT || 5000));
 
 
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
 
