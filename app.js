d0283a3ab36f0d0798d63d664ed2de42314b399d
Sanjeev Dasgupta <sanjeev_dasgupta@hotmail.com>
2022-03-11 13:13:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const http = require("http");
 const sequelize = require('./utils/database.js');
 
 const router = require('./routes/routes.js');
-
+const bodyParser = require("body-parser");
 const app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 
