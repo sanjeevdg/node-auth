561931bbffd68deed64e685e8cbcce7ddf69435a
Sanjeev Dasgupta <sanjeev_dasgupta@hotmail.com>
2022-03-22 20:05:14 +0530

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -1,8 +1,8 @@
 const {Sequelize} = require('sequelize');
 
-const sequelize = new Sequelize('sql6478058', 'sql6478058', '3VIQYqW9Yc', {
-    dialect: 'mysql',
-    host: 'sql6.freemysqlhosting.net'
+const sequelize = new Sequelize('d53r3v8g11u0i3', 'skjflmfrcwovac', 'ddede2349faac5d2bb95cfbc7198932ccaec5d84635a18b81557b66100b93a3f', {
+    dialect: 'postgres',
+    host: 'ec2-18-209-153-180.compute-1.amazonaws.com'
 });
 
 //export default sequelize;
